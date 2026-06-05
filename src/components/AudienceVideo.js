aef03d064cfa231d7af7eafe696213a5a326c1f3
jagseeun <s2468@e-mirim.hs.kr>
2026-06-05 11:20:42 +0900

    Play audience video during session

--- src/components/AudienceVideo.js
+++ src/components/AudienceVideo.js
@@ -1,13 +1,17 @@
-function AudienceVideo({ audience, className = '' }) {
+function AudienceVideo({ audience, className = '', playing = false }) {
   if (!audience?.videoSrc) {
     return <span className={`audience-fallback ${className}`}>{audience?.name ?? '청중'}</span>;
   }
 
+  const source = playing ? audience.videoSrc : `${audience.videoSrc}#t=0.1`;
+
   return (
     <video
       className={`audience-video ${className}`}
-      src={`${audience.videoSrc}#t=0.1`}
+      src={source}
       aria-label={audience.name.replace('\n', ' ')}
+      autoPlay={playing}
+      loop={playing}
       muted
       playsInline
       preload="metadata"
